4a8b7d3fc4d61a1780c6d067c27f10b8900a5a8d
Aerowinder <51070353+Aerowinder@users.noreply.github.com>
2022-12-07 14:25:01 -0500

    v3

--- Manage-Credential.ps1
+++ Manage-Credential.ps1
@@ -1,41 +1,40 @@
 $dir_cred = $env:userprofile + '\Credentials'
-New-Item -Path $dir_cred -ItemType Directory -Force
-
 $null = $exit #Suppress PSUseDeclaredVarsMoreThanAssignments code warning.
 $exit = $false
 
 function Show-Credential {
     Clear-Host
-    Write-Host 'Credentials Present:'
-    Write-Host
+    Write-Host 'Directory:' $dir_cred
+    
     $files_cred = Get-ChildItem $dir_cred
+    if ($null -ne $files_cred) {
+        Write-Host 'Credentials:' $files_cred.Count
+        Write-Host
+        foreach ($cred in $files_cred) {Write-Host ($cred.Name -Replace '.xml','') -ForegroundColor Yellow}
+    } else {
+        Write-Host 'Credentials: 0'
+    }
 
-    foreach ($cred in $files_cred) {Write-Host ($cred.Name -Replace '.xml','')}
     Write-Host
 }
 
+New-Item -Path $dir_cred -ItemType Directory -Force | Out-Null
+
 do {
-    Clear-Host
-    Write-Host 'c. Create credential'
-    Write-Host 'r. Remove credential'
-    Write-Host 'l. List credential'
+    Show-Credential
+    Write-Host '1. Create credential'
+    Write-Host '2. Remove credential'
     Write-Host
-    $opt = Read-Host 'Select Option (c, r, l; any other key to exit)'
+    $opt = Read-Host 'Select Option (1, 2). Press any other key to exit'
 
     switch ($opt) {
-        'c' {
-            Show-Credential
-            $cred_newname = Read-Host 'New Credential Name'
-            Get-Credential | Export-CliXml -Path  ($dir_cred + '\' + $cred_newname + '.xml')
+        '1' {
+            $userinput = Read-Host 'New Credential Name'
+            Get-Credential | Export-CliXml -Path  ($dir_cred + '\' + $userinput + '.xml')
         }
-        'r' {
-            Show-Credential
-            $cred_remove = Read-Host 'Remove Credential'
-            Remove-Item -Path ($dir_cred + '\' + $cred_remove + '.xml') -Confirm:$false
-        }
-        'l' {
-            Show-Credential
-            Read-Host 'Press any key to continue'
+        '2' {
+            $userinput = Read-Host 'Remove Credential'
+            Remove-Item -Path ($dir_cred + '\' + $userinput + '.xml') -Confirm:$false
         }
         default {$exit = $true}
     }
@@ -44,3 +43,4 @@ do {
 #Changelog
 #2022-12-02 - AS - v1 - Refactored for Git. Added listing and removal of credentials.
 #2022-12-07 - AS - v2 - Added loop to perform multiple actions per script execution. Updated variable names.
+#2022-12-07 - AS - v3 - Added directory and credential count to main screen. Removed list function; perform automatically instead.
